b969e30a4f9ed515cebb4294da718df982ca7f8e
Charani <charani@Charanis-MacBook-Air.local>
2025-01-28 18:19:47 +0530

    added put and delete end points

--- src/main/java/com/dataquadinc/controller/RequirementsController.java
+++ src/main/java/com/dataquadinc/controller/RequirementsController.java
@@ -2,24 +2,13 @@ package com.dataquadinc.controller;
 
 import java.util.List;
 
+import com.dataquadinc.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.dataquadinc.dto.RecruiterRequirementsDto;
-import com.dataquadinc.dto.RequirementAddedResponse;
-import com.dataquadinc.dto.RequirementsDto;
-import com.dataquadinc.dto.StatusDto;
 import com.dataquadinc.service.RequirementsService;
 @CrossOrigin(origins = "http://35.188.150.92")
 @RestController
@@ -60,5 +49,27 @@ public class RequirementsController {
 	public ResponseEntity<List<RecruiterRequirementsDto>> getJobsByRecruiter(@PathVariable String recruiterId) {
 		return new ResponseEntity<>(service.getJobsAssignedToRecruiter(recruiterId),HttpStatus.OK);
 	}
+	@PutMapping("/updateRequirement/{jobId}")
+	public ResponseEntity<ResponseBean> updateRequirement(@PathVariable String jobId, @RequestBody RequirementsDto requirementsDto) {
+		// Ensure that the jobId in the URL matches the jobId in the DTO
+		requirementsDto.setJobId(jobId);
+
+		// Call the service method to update the requirement
+		ResponseBean response = service.updateRequirementDetails(requirementsDto);
+
+		// Return the response entity with the appropriate status code
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/deleteRequirement/{jobId}")
+	public ResponseEntity<ResponseBean> deleteRequirement(@PathVariable String jobId) {
+		// Call the service method to delete the requirement by jobId
+		ResponseBean response = service.deleteRequirementDetails(jobId);
+
+		// Return the response entity with the appropriate status code
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+
 
 }
--- src/main/java/com/dataquadinc/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/dataquadinc/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.dataquadinc.exceptions;
 
 import java.time.LocalDateTime;
 
+import com.dataquadinc.dto.ResponseBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,12 +18,25 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<>(errorResponse, HttpStatus.OK);
 	}
 
-	@ExceptionHandler(RequirementNotFoundException.class)
-	public ResponseEntity<ErrorResponse> handleRequirementNotFound(RequirementNotFoundException ex) {
-		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.OK.value(), ex.getMessage(),
-				LocalDateTime.now());
-		return new ResponseEntity<>(errorResponse, HttpStatus.OK);
-	}
+//	@ExceptionHandler(RequirementNotFoundException.class)
+//	public ResponseEntity<ErrorResponse> handleRequirementNotFound(RequirementNotFoundException ex) {
+//		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.OK.value(), ex.getMessage(),
+//				LocalDateTime.now());
+//		return new ResponseEntity<>(errorResponse, HttpStatus.OK);
+//	}
+@ExceptionHandler(RequirementNotFoundException.class)
+public ResponseEntity<ResponseBean> handleRequirementNotFound(RequirementNotFoundException ex) {
+	// Create the custom response bean
+	ResponseBean response = new ResponseBean(
+			false,
+			ex.getMessage(),
+			ex.getClass().getSimpleName(),
+			null
+	);
+	// Return the ResponseEntity with 404 status (Not Found)
+	return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+}
+
 
 	@ExceptionHandler(NoJobsAssignedToRecruiterException.class)
 	public ResponseEntity<ErrorResponse> handleNoJobsAssignedToRecruiter(NoJobsAssignedToRecruiterException ex) {
@@ -37,4 +51,5 @@ public class GlobalExceptionHandler {
 				"An unexpected error occurred", LocalDateTime.now());
 		return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+
 }
