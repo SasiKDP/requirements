d816e3d1dd8fa42f1d099c212d26e56427cadd6b
SasiKDP <sasank9110@gmail.com>
2025-02-25 18:01:58 +0530

    commited.

--- src/main/java/com/dataquadinc/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/dataquadinc/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.dataquadinc.exceptions;
 
 import java.time.LocalDateTime;
 
+import com.dataquadinc.dto.ResponseBean;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,11 +12,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+
+
 	@ExceptionHandler(RequirementAlreadyExistsException.class)
 	public ResponseEntity<ErrorResponse> handleRequirementAlreadyExists(RequirementAlreadyExistsException ex) {
 		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.OK.value(), ex.getMessage(),
 				LocalDateTime.now());
 		return new ResponseEntity<>(errorResponse, HttpStatus.OK);
+	}
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+		// Return a meaningful response with a 400 Bad Request status
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(RequirementNotFoundException.class)
@@ -37,4 +46,14 @@ public class GlobalExceptionHandler {
 				"An unexpected error occurred", LocalDateTime.now());
 		return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<String> handleConstraintViolation(ConstraintViolationException ex) {
+		StringBuilder errorMessage = new StringBuilder();
+		ex.getConstraintViolations().forEach(violation -> {
+			errorMessage.append(violation.getMessage()).append("\n");
+		});
+		return new ResponseEntity<>(errorMessage.toString(), HttpStatus.BAD_REQUEST);
+	}
+
+
 }
