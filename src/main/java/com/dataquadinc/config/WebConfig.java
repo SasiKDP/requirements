c8567b3e9ba9a7a299e1753f914cfeae904c619f
SasiKDP <sasank9110@gmail.com>
2025-04-09 16:01:08 +0530

    Cloning main to master Again.

--- src/main/java/com/dataquadinc/config/WebConfig.java
+++ src/main/java/com/dataquadinc/config/WebConfig.java
@@ -17,11 +17,11 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://192.168.0.139:3000", // Third IP
                         "https://mymulya.com", // Forth IP
                         "http://localhost:3000", // Fifth IP
-                        "http://192.168.0.135:8080",
+                        "http://192.168.0.135/",
                         "http://192.168.0.135:80","http://182.18.177.16:443",
-                        "https://mymulya.com:443",
                         "http://mymulya.com:443",
-                        "http://localhost/"// Sixth IP
+                        "http://localhost/",
+                        "http://182.18.177.16/" // Sixth IP
                 )
 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
