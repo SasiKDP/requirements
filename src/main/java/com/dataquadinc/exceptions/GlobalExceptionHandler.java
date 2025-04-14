269e7798cfcf135c5c5b559e01d863d7f4656237
SasiKDP <sasank9110@gmail.com>
2025-04-14 10:07:07 +0530

    fixed the column too long for the job description

--- src/main/java/com/dataquadinc/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/dataquadinc/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.dataquadinc.exceptions;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -60,4 +62,5 @@ public class GlobalExceptionHandler {
 		error.put("error", ex.getMessage());
 		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
 	}
+
 }
