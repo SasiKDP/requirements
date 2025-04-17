2d97ac32872dae92cef479e0b19f7fce0761a4ad
SasiKDP <sasank9110@gmail.com>
2025-04-17 19:07:16 +0530

    combined the employees count and the team lead count into single count.

---
+++ src/main/java/com/dataquadinc/dto/UserStatsDTO.java
@@ -0,0 +1,148 @@
+package com.dataquadinc.dto;
+
+
+public class UserStatsDTO {
+
+    private String employeeId;
+    private String employeeName;
+    private String employeeEmail;
+    private String role;
+
+    private int numberOfClients;
+    private int numberOfRequirements;
+
+    // Employee-specific
+    private int numberOfSubmissions;
+    private int numberOfInterviews;
+    private int numberOfPlacements;
+
+    // Teamlead-specific
+    private int selfSubmissions;
+    private int selfInterviews;
+    private int selfPlacements;
+    private int teamSubmissions;
+    private int teamInterviews;
+    private int teamPlacements;
+
+    // Getters and setters...
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeEmail() {
+        return employeeEmail;
+    }
+
+    public void setEmployeeEmail(String employeeEmail) {
+        this.employeeEmail = employeeEmail;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getNumberOfClients() {
+        return numberOfClients;
+    }
+
+    public void setNumberOfClients(int numberOfClients) {
+        this.numberOfClients = numberOfClients;
+    }
+
+    public int getNumberOfRequirements() {
+        return numberOfRequirements;
+    }
+
+    public void setNumberOfRequirements(int numberOfRequirements) {
+        this.numberOfRequirements = numberOfRequirements;
+    }
+
+    public int getNumberOfSubmissions() {
+        return numberOfSubmissions;
+    }
+
+    public void setNumberOfSubmissions(int numberOfSubmissions) {
+        this.numberOfSubmissions = numberOfSubmissions;
+    }
+
+    public int getNumberOfInterviews() {
+        return numberOfInterviews;
+    }
+
+    public void setNumberOfInterviews(int numberOfInterviews) {
+        this.numberOfInterviews = numberOfInterviews;
+    }
+
+    public int getNumberOfPlacements() {
+        return numberOfPlacements;
+    }
+
+    public void setNumberOfPlacements(int numberOfPlacements) {
+        this.numberOfPlacements = numberOfPlacements;
+    }
+
+    public int getSelfSubmissions() {
+        return selfSubmissions;
+    }
+
+    public void setSelfSubmissions(int selfSubmissions) {
+        this.selfSubmissions = selfSubmissions;
+    }
+
+    public int getSelfInterviews() {
+        return selfInterviews;
+    }
+
+    public void setSelfInterviews(int selfInterviews) {
+        this.selfInterviews = selfInterviews;
+    }
+
+    public int getSelfPlacements() {
+        return selfPlacements;
+    }
+
+    public void setSelfPlacements(int selfPlacements) {
+        this.selfPlacements = selfPlacements;
+    }
+
+    public int getTeamSubmissions() {
+        return teamSubmissions;
+    }
+
+    public void setTeamSubmissions(int teamSubmissions) {
+        this.teamSubmissions = teamSubmissions;
+    }
+
+    public int getTeamInterviews() {
+        return teamInterviews;
+    }
+
+    public void setTeamInterviews(int teamInterviews) {
+        this.teamInterviews = teamInterviews;
+    }
+
+    public int getTeamPlacements() {
+        return teamPlacements;
+    }
+
+    public void setTeamPlacements(int teamPlacements) {
+        this.teamPlacements = teamPlacements;
+    }
+}
